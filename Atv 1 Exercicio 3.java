5b1b3b97e14ddf393755e405dcbdd3e0e45c0bd4
TMarceloLPS <marcelo.tadeu.lps@gmail.com>
2025-03-26 20:29:50 -0300

    Create Atv 1 Exercicio 3.java

---
+++ Atv 1 Exercicio 3.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+class Testes {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B , C , D, F;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		F = (A * B - C * D);
+		System.out.println("Diferenca =" + F);
+		
+		
+		
+	}
